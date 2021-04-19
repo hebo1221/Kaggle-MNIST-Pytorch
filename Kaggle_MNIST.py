acda9661b647c358d17612e7e7bb88afe57a804d
junghun <hebo1221@gmail.com>
2021-04-19 16:12:43 +0900

    Add files via upload

---
+++ Kaggle_MNIST.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from torchvision.utils import make_grid
+
+import math
+import random
+
+from PIL import Image, ImageOps, ImageEnhance
+import numbers
+
+import matplotlib.pyplot as plt
+
+train_df = pd.read_csv('../input/train.csv')
+
+n_train = len(train_df)
+n_pixels = len(train_df.columns) - 1
+n_class = len(set(train_df['label']))
+
+print('Number of training samples: {0}'.format(n_train))
+print('Number of training pixels: {0}'.format(n_pixels))
+print('Number of classes: {0}'.format(n_class))
